caa025dd82eaa7fa59546e124e3b18b26bba53e2
jgroma <jgromadzinska9@gmail.com>
2024-07-07 17:35:41 +0100

    Set up project

---
+++ src/main/java/com/jgroma/accountability_shepherd/AccountabilityShepherdApplication.java
@@ -0,0 +1,13 @@
+package com.jgroma.accountability_shepherd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountabilityShepherdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountabilityShepherdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgroma/accountability_shepherd/AccountabilityShepherdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgroma.accountability_shepherd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountabilityShepherdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
